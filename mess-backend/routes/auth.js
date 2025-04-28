ba3a5c5b4fa20865edad1f94cb0ec258d1c8d9c9
Santhosh T <santhoshthirumoorthy890@gmail.com>
2025-04-28 12:14:34 +0530

    testing backend

--- mess-backend/routes/auth.js
+++ mess-backend/routes/auth.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const { loginUser, registerUser } = require('../controllers/authController');
 
+
+
+// Temporary test route
+router.get('/', (req, res) => {
+  res.send('Auth API Working!');
+});
+
+// Your existing routes...
+// router.post('/login', ...)
+// router.post('/register', ...)
+
+module.exports = router;
+
 router.post('/login', loginUser);
 router.post('/register', registerUser);
 
