ddba6910d7ae1ab6a72fcaae3b90db1294f6191f
Santhosh T <santhoshthirumoorthy890@gmail.com>
2025-05-01 13:35:30 +0530

    configured frontend

--- mess-frontend/src/api/axiosInstance.js
+++ mess-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://3.92.61.78:5000/api',
+  baseURL: 'http://52.2.93.132:5000/api',
 });
 
 API.interceptors.request.use((req) => {
